fe11d39e8841e5481b96e7460df8abd63643c175
Dat <dyoutobe1001@gmail.com>
2026-08-17 11:01:06 +0700

    index
    index

---
+++ js/common.js
@@ -0,0 +1,294 @@
+// menu toggle
+$(function () {
+    var html = $('html, body'),
+        navContainer = $('.nav-container'),
+        navToggle = $('.nav-toggle'),
+        navDropdownToggle = $('.has-dropdown');
+    overlay = $("<div class='overlay'></div> ");
+    overlay2 = $("<div class='overlay'></div> ");
+
+    // Nav toggle
+    navToggle.on('click', function (e) {
+        overlay.toggle();
+        var $this = $(this);
+        e.preventDefault();
+        $this.toggleClass('is-active');
+        navContainer.toggleClass('is-visible');
+        html.toggleClass('nav-open');
+    });
+
+
+    $("body").prepend(overlay);
+    overlay.click(function () {
+        navToggle.trigger('click');
+        // $(this).toggle();
+    })
+
+    $("body").prepend(overlay2);
+    overlay2.click(function () {
+        $(this).toggle();
+    })
+    // Nav dropdown toggle
+    navDropdownToggle.on('click', function () {
+        var $this = $(this);
+        $this.toggleClass('is-active').siblings().removeClass('is-active'); 
+        if ($this.children('ul').hasClass('open-nav')) {
+            $this.children('ul').removeClass('open-nav');
+            $this.children('ul').slideUp(350);
+        }
+        else {
+            $this.parent().parent().find('li .nav-dropdown').removeClass('open-nav');
+            $this.parent().parent().find('li .nav-dropdown').slideUp(350);
+            $this.children('ul').toggleClass('open-nav');
+            $this.children('ul').slideToggle(350);
+        }
+    });
+
+    // Prevent click events from firing on children of navDropdownToggle
+    navDropdownToggle.on('click', '*', function (e) {
+        e.stopPropagation();
+    });
+
+
+});
+
+//scroll to top button
+// ----------- croll --------------//
+(function ($) {
+    //Scroll to Top
+    function headerStyle() {
+        if ($('.header').length) {
+            var windowpos = $(window).scrollTop();
+            var scrollLink = $('.scroll-top');
+            if (windowpos >= 185) {
+                scrollLink.addClass('open');
+            } else {
+                scrollLink.removeClass('open');
+            }
+        }
+    }
+    headerStyle();
+    // Scroll to Target
+    if ($('.scroll-to-target').length) {
+        $(".scroll-to-target").on('click', function () {
+            var target = $(this).attr('data-target');
+            // animate
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 1000);
+
+        });
+    }
+
+    $(window).on('scroll', function () {
+        headerStyle();
+    });
+
+
+})(window.jQuery);
+$(".has-dropdown-1 .menu-link").click(function () {
+    $(".product-dropdown").toggleClass("active");
+});
+
+// click language search
+$( ".box-search i" ).on( "click", function() {
+    $( ".search-pc" ).toggle();
+});
+//  fixed header 
+$(window).on("scroll resize", function () {
+  if (window.innerWidth >= 992) {
+    if ($(document).scrollTop() > 10) {
+      $(".header").addClass("fixd-header");
+    } else {
+      $(".header").removeClass("fixd-header");
+    }
+  } else {
+    // Nếu dưới 992px thì luôn gỡ bỏ class (nếu có)
+    $(".header").removeClass("fixd-header");
+  }
+});
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $(".menu_mb").addClass("fixd-header-mobile");
+    } else {
+      $(".menu_mb").removeClass("fixd-header-mobile");
+    }
+});
+
+//Featured
+
+$('.car-card__bar').on('mouseenter', function () { 
+    var image = $(this).data('image');
+    var $card = $(this).closest('.car-card');
+    var $image = $card.find('.car-card__image'); 
+    $image.css('opacity', 0); 
+    setTimeout(function () { 
+        $image.attr('src', image); 
+        $image.css('opacity', 1); 
+    }, 150);
+
+});
+
+
+$('.car-card__bar').on('mouseleave', function () { 
+    var $card = $(this).closest('.car-card'); 
+    var $active = $card.find('.car-card__bar.active'); 
+    var image = $active.data('image'); 
+    var $image = $card.find('.car-card__image'); 
+    $image.css('opacity', 0); 
+    setTimeout(function () { 
+        $image.attr('src', image); 
+        $image.css('opacity', 1); 
+    }, 150);
+
+});
+
+// end Featured
+
+// slider
+
+$(".slider").owlCarousel({
+    items: 1,
+    responsive: {
+        1200: { items: 1 },
+        992: { items: 1 },
+        768: { items: 1 },
+        480: { items: 1 },
+        0: { items: 1 }
+    },
+    rewind: false,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 3000, //slide speed smooth
+    dots: false,
+    dotsEach: false,
+    loop: true,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left arrow-slider"></i>', '<i class="fa fa-angle-right arrow-slider"></i>'],
+    margin:0,
+    animateOut: ['fadeOut'], // default: false
+    animateIn: ['fadeIn'], // default: false
+    center: false,
+});
+// end slider 
+$(document).ready(function () { 
+        var $section = $('#company-stats');
+        var hasAnimated = false;
+
+        function startCounters() {
+
+            if (hasAnimated) {
+                return;
+            }
+
+            hasAnimated = true;
+
+            $section.find('.counter').each(function () {
+
+                var $this = $(this);
+                var target = parseInt($this.attr('data-target'), 10);
+
+                $({ count: 0 }).animate(
+                    {
+                        count: target
+                    },
+                    {
+                        duration: 2000,
+
+                        easing: 'swing',
+
+                        step: function (now) {
+                            $this.text(
+                                Math.floor(now).toLocaleString('en-US')
+                            );
+                        },
+
+                        complete: function () {
+                            $this.text(
+                                target.toLocaleString('en-US')
+                            );
+                        }
+                    }
+                );
+
+            });
+        }
+
+
+        function checkStatsPosition() {
+
+            var windowTop = $(window).scrollTop();
+            var windowHeight = $(window).height();
+
+            var sectionTop = $section.offset().top;
+
+            // Khi phần tử đi vào khoảng 80% viewport
+            if (
+                windowTop + windowHeight * 0.8 >= sectionTop
+            ) {
+                startCounters();
+            }
+        }
+
+
+        $(window).on('scroll', checkStatsPosition);
+
+        // Kiểm tra luôn khi trang vừa load
+        checkStatsPosition();
+
+    });
+
+// slider-car-card
+
+$(".slider-car-card").owlCarousel({
+    items: 3,
+    responsive: {
+        1200: { items: 3, slideBy: 3},
+        992: { items: 3, slideBy: 3},
+        768: { items: 2, slideBy: 2},
+        480: { items: 1, slideBy: 1}, 
+        0: { items: 1 }
+    },
+    rewind: false,
+    autoplay: false,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    //smartSpeed: 3000, //slide speed smooth
+    dots: true,
+    dotsEach: false,
+    loop: true,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left arrow-slider"></i>', '<i class="fa fa-angle-right arrow-slider"></i>'],
+    margin:30,  
+    //animateOut: ['fadeOut'], // default: false
+    //animateIn: ['fadeIn'], // default: false
+    center: false,
+});
+
+
+$(".slider-panner").owlCarousel({
+    items: 6,
+    responsive: {
+        1200: { items: 6},
+        992: { items: 5},
+        768: { items: 4},
+        480: { items: 3}, 
+        0: { items: 2 }
+    },
+    rewind: false,
+    autoplay: false,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    //smartSpeed: 3000, //slide speed smooth
+    dots: false,
+    dotsEach: false,
+    loop: true,
+    nav: false,
+    //navText: ['<i class="fa fa-angle-left arrow-slider"></i>', '<i class="fa fa-angle-right arrow-slider"></i>'],
+    margin:30,  
+    //animateOut: ['fadeOut'], // default: false
+    //animateIn: ['fadeIn'], // default: false
+    center: false,
+});
+
